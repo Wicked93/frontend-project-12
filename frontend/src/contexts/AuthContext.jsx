45a3dd306b96394a2e9eb105422c4b567434d1cd
Anton Zlobin <103571383+Wicked93@users.noreply.github.com>
2023-03-26 10:10:00 +0500

    Step 8

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext({});
 
-// eslint-disable-next-line import/prefer-default-export
 export const AuthProvider = ({ children }) => {
   const userLog = JSON.parse(localStorage.getItem('userId'));
   const [loggedIn, setLoggedIn] = useState(userLog ? { username: userLog.username } : null);
@@ -16,14 +16,6 @@ export const AuthProvider = ({ children }) => {
     setLoggedIn(null);
   };
 
-  const getAuthToken = () => {
-    const currentUser = userLog;
-    if (currentUser && currentUser.token) {
-      return { Authorization: `Basic ${currentUser.token}` };
-    }
-    return {};
-  };
-
   const getUserName = () => {
     if (userLog) {
       return userLog.username;
@@ -31,10 +23,18 @@ export const AuthProvider = ({ children }) => {
     return null;
   };
 
+  const getAuthToken = () => {
+    const currentUser = userLog;
+    if (currentUser && currentUser.token) {
+      return { Authorization: `Bearer ${currentUser.token}` };
+    }
+    return {};
+  };
+
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
     <AuthContext.Provider value={{
-      loggedIn, logIn, logOut, getAuthToken, getUserName, userLog,
+      loggedIn, logIn, logOut, getUserName, getAuthToken, userLog,
     }}
     >
       {children}
